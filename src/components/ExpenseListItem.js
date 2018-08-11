af5fdef1f059b5a1a17f46d8715ff9c4eca4a855
TonyChen <tonychenn@gmail.com>
2018-08-11 14:51:08 +0800

    格式化createdAt和amount的顯示

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = props => (
     <div>
         <Link to={`/edit/${props.id}`}><h3>Description: {props.description}</h3></Link>
-        <p>Note: {props.amount}-{props.createdAt}</p>
+        <p>Note: 
+        {numeral(props.amount / 100).format('$0,0.00')}
+        -
+        {moment(props.createdAt).format('MMMM Do YYYY')}</p>
     </div>
 );
 
