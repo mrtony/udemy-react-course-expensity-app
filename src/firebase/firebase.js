cc8272df4e1266d37d319ca3ff54311bfcce9864
TonyChen <tonychenn@gmail.com>
2018-08-13 00:52:26 +0800

    新增測試資料庫的設定並完成環境設定

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import database from 'firebase/database';
 import moment from 'moment';
 
 const config = {
-  apiKey: "AIzaSyAbJEDf5nvg5qzIHyCAljbTt4kGPRQG4R0",
-  authDomain: "expensity-92a10.firebaseapp.com",
-  databaseURL: "https://expensity-92a10.firebaseio.com",
-  projectId: "expensity-92a10",
-  storageBucket: "expensity-92a10.appspot.com",
-  messagingSenderId: "128856949432"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
