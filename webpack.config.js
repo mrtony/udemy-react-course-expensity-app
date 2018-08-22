a15fabee46b305da64978471f8c08a582d4afbfa
TonyChen <tonychenn@gmail.com>
2018-08-22 20:36:20 +0800

    add babel-polyfill for IE11

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = (env, argv) => {
   const isProdction = env === 'production';
   const CSSExtract = new ExtractTextPlugin({filename: 'styles.css'})
   return {
-    entry: './src/app.js',
+    entry: ['babel-polyfill', './src/app.js'],
     output: {
         path: path.join(__dirname, 'public', 'dist'),
         filename: 'bundle.js'
